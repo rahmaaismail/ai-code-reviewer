0edc47f21caad6491b5f90b254ab41b7a001d13a
Rahma Ismail <rahmaismail@uvic.ca>
2026-05-06 20:52:52 -0700

    Updating files to match next.js version

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,17 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['DM Sans', 'sans-serif'],
+        mono: ['Geist Mono', 'monospace'],
+      },
+    },
+  },
   plugins: [],
 };
 
